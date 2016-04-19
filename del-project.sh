e16f514c8d6caa08336caac41f29202539441154
root <root@node-63.mgmt.ssclab.com>
2016-04-19 19:43:11 +0000

    Add validation of deletion before doing the job just in case...

--- del-project.sh
+++ del-project.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Take tenant name as an argument
+read -p "Are you sure you want to delete project $1? There is no way back [Y or N]: " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuffa
+    dirname=`dirname $0`
+    tenant=`$dirname/get-tenantid.sh $1`
 
-dirname=`dirname $0`
-tenant=`$dirname/get-tenantid.sh $1`
-
-$dirname/del-vm.sh $tenant
-$dirname/del-router.sh $tenant
-$dirname/del-net.sh $tenant
+    $dirname/del-vm.sh $tenant
+    $dirname/del-router.sh $tenant
+    $dirname/del-net.sh $tenant
+    openstack project delete $1
+    openstack user delete $1
+fi
